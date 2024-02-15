ba3ff8f5a39157b5bb90df7a71a630e56f5401c0
gkontogiannhs <g.kontogiannis@ac.upatras.gr>
2024-02-15 13:53:16 +0000

    add FE transformer to init

--- caits/transformers/__init__.py
+++ caits/transformers/__init__.py
@@ -1,3 +1,5 @@
 from ._data_object import CAI
 from ._transformer import Transformer
 from ._sliding_window import SlidingWindow
+from ._augmentation import Augmenter
+from ._feature_extractor import FeatureExtractor
