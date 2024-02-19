d485c135ff69e5efbfb88df80905dcf2503dd6b1
Pantelis Tzamalis <tzamalispantelis@gmail.com>
2024-02-19 16:35:03 +0200

    CTS-3 add power spectrogram calculation

--- caits/fe/_spectrum.py
+++ caits/fe/_spectrum.py
@@ -42,3 +42,67 @@ def compute_spectrogram(
                       noverlap=noverlap, nfft=nfft, boundary=None)
     freq_mask = (f >= fmin) & (f <= fmax)
     return f[freq_mask], t, np.abs(spec[freq_mask, :])
+
+
+def compute_power_spectrogram(
+        signal,
+        fs,
+        window='hann',
+        nperseg=256,
+        noverlap=None,
+        nfft=None,
+        fmin: float = None,
+        fmax: float = None
+):
+    """Computes the power spectrogram of a signal.
+
+    Args:
+        signal: array_like
+            Input signal.
+        fs: float
+            Sampling frequency of the signal.
+        window: str or tuple or array_like, optional
+            Desired window to use. Default is 'hann'.
+        nperseg: int, optional
+            Length of each segment. Defaults to 256.
+        noverlap: int, optional
+            Number of points to overlap between segments. If None, `nperseg // 2` is used.
+        nfft: int, optional
+            Length of the FFT used, if a zero-padded FFT is desired. If None, it defaults to `nperseg`.
+        fmin (Optional): The lowest frequency to include in the spectrogram
+            (in Hz). If None, it defaults to 0.
+        fmax (Optional): The highest frequency to include in the spectrogram
+            (in Hz).  If None, it defaults to `fs / 2.0`.
+
+    Returns:
+        f: ndarray
+            Array of sample frequencies.
+        t: ndarray
+            Array of segment times.
+        Pxx: ndarray
+            Power spectrogram of the signal.
+    """
+    if fmin is None:
+        fmin = 0
+    if fmax is None:
+        fmax = fs / 2.0
+
+    f, t, Zxx = stft(signal, fs=fs, window=window, nperseg=nperseg, noverlap=noverlap, nfft=nfft)
+    freq_mask = (f >= fmin) & (f <= fmax)
+    f = f[freq_mask]
+    spec = np.abs(Zxx[freq_mask, :])
+    Pxx = np.abs(spec)**2
+    return f, t, Pxx
+
+
+def transform_to_power_spectrogram(
+        spec: np.ndarray
+) -> np.ndarray:
+    """Transforms a complex-valued spectrogram to a power spectrogram.
+
+    Args: Input complex-valued spectrogram in np.ndarray.
+
+    Returns:
+        mp.ndarray: The power spectrogram.
+    """
+    return np.abs(spec)**2
