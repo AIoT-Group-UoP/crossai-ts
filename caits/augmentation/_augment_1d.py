96c45dc44784f67c08aebf62dec0f005e8ca3d6f
Pantelis Tzamalis <tzamalispantelis@gmail.com>
2024-02-21 20:30:57 +0200

    CTS-18 add random gain, polarity inversion

--- caits/augmentation/_augment_1d.py
+++ caits/augmentation/_augment_1d.py
@@ -16,3 +16,34 @@ def add_white_noise(
     """
     noise = np.random.normal(0, array.std(), array.size)
     return array + noise_factor * noise
+
+
+def random_gain(
+        array: np.ndarray,
+        min_factor: float = 0.1,
+        max_factor: float = 0.12
+) -> np.ndarray:
+    """Applies random gain to a signal.
+
+    Args:
+        array: The input signal.
+        min_factor: The minimum gain factor.
+        max_factor: The maximum gain factor.
+
+    Returns:
+        ndarray: The signal with random gain applied.
+    """
+    gain_rate = np.random.uniform(min_factor, max_factor)
+    return array * gain_rate
+
+
+def polarity_inversion(array: np.ndarray) -> np.ndarray:
+    """Inverts the polarity of a signal.
+
+    Args:
+        array: The input signal.
+
+    Returns:
+        ndarray: The signal with inverted polarity.
+    """
+    return array * -1
