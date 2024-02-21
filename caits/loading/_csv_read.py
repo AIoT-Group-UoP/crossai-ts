58de2e9b71996b1f5831831f8af065b06c4c55b9
gkontogiannhs <g.kontogiannis@ac.upatras.gr>
2024-02-21 19:14:40 +0000

    feat: add csv loader matching audio loader flow

--- caits/loading/_csv_read.py
+++ caits/loading/_csv_read.py
@@ -1,49 +1,54 @@
+import os
 import pandas as pd
-from typing import Union
+import glob
+from tqdm import tqdm
+from typing import Union, List
 
 
 def csv_loader(
         dataset_path: str,
         header: Union[None, int, str] = "infer",
-        channels: Union[list, None] = None,
+        channels: Union[List[str], None] = None,
         export: str = "dict"
 ) -> Union[pd.DataFrame, dict]:
-    """Loads CSV files from a directory into a DataFrame.
+    """Loads CSV files from a directory into a DataFrame or dictionary.
 
     Args:
-        dataset_path: Path to the directory containing subdirectories with the
-            CSV files.
-        header:
-        channels: List of column names to be used in the DataFrame. Default is
-            None, which will use the column names from the CSV file.
-        export: A boolean that defines the export format. Default is "dict".
-            It can be "dict" or "df".
+        dataset_path: Path to the dataset directory containing CSV files.
+        header: Specifies the row(s) to use as the column names.
+                Defaults to "infer".
+        channels: List of column names to use. If None, all columns are used.
+        export: Format to export the loaded data, "dict" or "df" for DataFrame.
 
     Returns:
-
+        pd.DataFrame or dict: Loaded CSV data.
     """
-    import os
-
     all_features = []
     all_y = []
     all_id = []
 
-    for subdir in os.listdir(dataset_path):
-        if subdir != ".DS_Store":
-            print("loading files in dir:", subdir)
-            label_path = os.path.join(dataset_path, subdir)
-            if os.path.isdir(label_path):
-                for file in os.listdir(label_path):
-                    file_path = os.path.join(label_path, file)
-                    try:
-                        df = pd.read_csv(file_path, header=header,
-                                         names=channels)
-                        all_features.append(df)
-                        all_y.append(subdir)
-                        all_id.append(file)
-                    except Exception as e:
-                        print(f"Error loading file {file_path}: {e}")
+    # Generate a search pattern to find CSV files in the dataset directory
+    search_pattern = os.path.join(dataset_path, "**", "*.csv")
+    file_paths = glob.glob(search_pattern, recursive=True)
 
+    for file_path in tqdm(file_paths, desc="Loading CSV files"):
+        subdir = os.path.basename(os.path.dirname(file_path))
+        file = os.path.basename(file_path)
+        try:
+            # Load the CSV file, specifying header and column names if provided
+            if channels is not None:
+                df = pd.read_csv(file_path, header=header, usecols=channels)
+            else:
+                df = pd.read_csv(file_path, header=header)
+
+            all_features.append(df)
+            all_y.append(subdir)
+            all_id.append(file)
+        except Exception as e:
+            print(f"Error loading file {file_path}: {e}")
+
+    # Export the loaded data as a DataFrame or
+    # dictionary based on the 'export' argument
     if export == "df":
         return pd.DataFrame({
             "X": all_features,
