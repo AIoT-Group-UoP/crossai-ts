38d9a9aa3b8fe76a464441f60e4736962c5ac46d
gkontogiannhs <g.kontogiannis@ac.upatras.gr>
2024-02-23 12:55:04 +0000

    add json_loader in init

--- caits/loading/__init__.py
+++ caits/loading/__init__.py
@@ -2,4 +2,4 @@ from ._audio import audio_loader
 from ._s3_audio import s3_audio_loader
 from ._csv_read import csv_loader
 from ._s3_csv_read import s3_csv_loader
-from ._utils import load_yaml_config
+from ._utils import load_yaml_config, json_loader
