6cfc2197fe689e2437136df51d6903ef22afa14a
Pantelis Tzamalis <tzamalispantelis@gmail.com>
2024-03-04 14:39:02 +0200

    add wav checker to init

--- caits/loading/__init__.py
+++ caits/loading/__init__.py
@@ -1,4 +1,4 @@
-from ._audio import audio_loader
+from ._audio import audio_loader, wav_specs_check
 from ._s3_audio import s3_audio_loader
 from ._csv_read import csv_loader
 from ._s3_csv_read import s3_csv_loader
