c1b5a4e4a83efb3cd55e970a218fb5dc759b33f0
gkontogiannhs <g.kontogiannis@ac.upatras.gr>
2024-03-04 18:51:49 +0200

    rename to 1D

--- caits/transformers/__init__.py
+++ caits/transformers/__init__.py
@@ -1,4 +1,4 @@
 from ._transformer import Transformer
 from ._sliding_window import SlidingWindow
-from ._augmentation import Augmenter
+from ._augmentation_1d import Augmenter1D
 from ._feature_extractor import FeatureExtractor
