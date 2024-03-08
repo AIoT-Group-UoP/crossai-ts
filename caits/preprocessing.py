66696383bd42104798fd896ee1d626ad18ce5ecb
Pantelis Tzamalis <tzamalispantelis@gmail.com>
2024-03-08 20:05:06 +0200

    CTS-7 feat: add dBFS calculation

--- caits/preprocessing.py
+++ caits/preprocessing.py
@@ -1,4 +1,6 @@
 import numpy as np
+from math import log, ceil
+from functools import reduce
 
 
 def normalize_signal(
@@ -113,3 +115,72 @@ def trim_signal(
     stop = length - stop
 
     return array[start:stop]
+
+
+def create_chunks(
+        array: np.ndarray,
+        chunk_length: int
+) -> list[np.ndarray]:
+    """
+
+    Args:
+        array:
+        chunk_length:
+
+    Returns:
+
+    """
+    n_chunks = ceil(len(array) / float(chunk_length))
+    return [array[i * chunk_length:(i + 1) * chunk_length]
+            for i in range(int(n_chunks))]
+
+
+def dBFS(
+        array: np.ndarray,
+        sample_width: int
+) -> float:
+    """Calculates the decibels relative to full scale (dBFS) of an audio.
+
+    Args:
+        array:
+        sample_width:
+
+    Returns:
+
+    """
+    from caits.fe._statistical import rms_value
+    rms = rms_value(array)
+    if not rms:
+        return -float("infinity")
+    return ratio_to_db(rms / max_possible_amplitude(sample_width))
+
+
+def max_possible_amplitude(
+        sample_width: int
+) -> float:
+    bits = sample_width * 8
+    max_possible_val = (2 ** bits)
+
+    # since half is above 0 and half is below the max amplitude is divided
+    return max_possible_val / 2
+
+
+def ratio_to_db(ratio, val2=None, using_amplitude=True):
+    """
+    Converts the input float to db, which represents the equivalent
+    to the ratio in power represented by the multiplier passed in.
+    """
+    ratio = float(ratio)
+
+    # accept 2 values and use the ratio of val1 to val2
+    if val2 is not None:
+        ratio = ratio / val2
+
+    # special case for multiply-by-zero (convert to silence)
+    if ratio == 0:
+        return -float('inf')
+
+    if using_amplitude:
+        return 20 * log(ratio, 10)
+    else:  # using power
+        return 10 * log(ratio, 10)
