f2126acefc557b75d10eafa07168cdb0bcfccbef
gkontogiannhs <g.kontogiannis@ac.upatras.gr>
2024-03-21 19:41:50 +0200

    modify init

--- caits/transformers/__init__.py
+++ caits/transformers/__init__.py
@@ -1,4 +1,5 @@
-from ._transformer import Transformer
+from ._func_transformer import Transformer
 from ._sliding_window import SlidingWindow
 from ._augmentation_1d import Augmenter1D
 from ._feature_extractor import FeatureExtractor
+from ._utils import ToSklearn 
