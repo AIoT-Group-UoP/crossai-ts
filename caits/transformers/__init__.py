326225a26723822579d9c2722cda8c7ea3f7ee39
gkontogiannhs <g.kontogiannis@ac.upatras.gr>
2024-03-29 13:58:20 +0200

    add serialization function to init

--- caits/transformers/__init__.py
+++ caits/transformers/__init__.py
@@ -2,5 +2,5 @@ from ._func_transformer import FunctionTransformer
 from ._sliding_window import SlidingWindow
 from ._augmentation_1d import Augmenter1D
 from ._feature_extractor import FeatureExtractor
-from ._utils import ToSklearn
+from ._utils import ToSklearn, sklearn_to_pkl
 from ._encoder import LE
