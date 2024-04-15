dbb3b82294adcd2b7b7d9de298cc202008a3e609
gkontogiannhs <g.kontogiannis@ac.upatras.gr>
2024-04-15 19:00:32 +0300

    feat: Add 2D feature extractor

---
+++ caits/transformers/_feature_extractor_2d.py
@@ -0,0 +1,38 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+import numpy as np
+from pandas import DataFrame
+from typing import List, Dict
+from caits.dataset import Dataset
+from collections import defaultdict
+
+
+class FeatureExtractor2D(BaseEstimator, TransformerMixin):
+    def __init__(self, feature_extractor: Dict):
+        self.feature_extractor = feature_extractor
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X: Dataset) -> Dataset:
+        transformed_X = []
+
+        for df in X.X:
+            # Ensure the DataFrame has only one column
+            if df.shape[1] != 1:
+                raise ValueError(f"2D Extractor does not support multi-channel signals yet. \
+                                 Expected single-channel signals.")
+            
+            func = self.feature_extractor["func"]
+            params = self.feature_extractor.get("params", {})
+
+            # Extract information
+            feature = func(df.values.flatten(), **params)
+
+            # Ensure feature is a 2D array
+            if feature.ndim != 2:
+                raise ValueError("Expected a 2D array.")
+            
+            # Store the 2D numpy array as a DataFrame
+            transformed_X.append(DataFrame(feature))
+
+        return Dataset(transformed_X, X.y, X._id)
