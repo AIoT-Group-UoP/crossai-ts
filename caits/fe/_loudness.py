0fed107925ef77401cba94b7b0dd9c514c3a2864
Pantelis Tzamalis <tzamalispantelis@gmail.com>
2024-05-20 11:51:16 +0300

    CTS-62 fix: rms with for loop replaced with caits rms_value function

--- caits/fe/_loudness.py
+++ caits/fe/_loudness.py
@@ -16,35 +16,10 @@ def dBFS(
     Returns:
 
     """
-    rms = rms_dbfs(array)
+    rms = rms_value(array)
     if not rms:
         return -float("infinity")
     return ratio_to_db(rms / max_possible_amplitude(sample_width))
-
-
-def rms_dbfs(arr: np.ndarray) -> float:
-    """Calculates the root mean square of an audio signal using math module.
-
-    Args:
-        arr: The input audio signal as a numpy.ndarray.
-
-    Returns:
-        float: The root mean square of the audio signal.
-    """
-    square = 0
-    mean = 0.0
-    root = 0.0
-
-    # Calculate square
-    n = len(arr)
-    for i in range(0, n):
-        square += (arr[i] ** 2)
-    # Calculate Mean
-    mean = (square / (float)(n))
-    # Calculate Root
-    root = math.sqrt(mean)
-
-    return root
 
 
 def max_possible_amplitude(
