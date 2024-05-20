45fccd9f94e15d53afa0acc0c8713dad3cb6743a
gkontogiannhs <g.kontogiannis@ac.upatras.gr>
2024-05-20 16:31:06 +0300

    CTS-63 fix: Update init file

--- caits/transformers/__init__.py
+++ caits/transformers/__init__.py
@@ -2,6 +2,6 @@ from ._func_transformer import FunctionTransformer
 from ._sliding_window import SlidingWindow
 from ._augmentation_1d import Augmenter1D
 from ._feature_extractor import FeatureExtractor
-from ._utils import DatasetToArray, ArrayToDataset, sklearn_to_pkl
 from ._encoder import LE
 from ._feature_extractor_2d import FeatureExtractor2D
+from ._data_converters import DatasetToArray, ArrayToDataset
