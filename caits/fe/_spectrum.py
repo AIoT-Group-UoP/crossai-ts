945f6a85af57589a943b4ba1994e7e397abfbe72
gkontogiannhs <g.kontogiannis@ac.upatras.gr>
2024-05-22 17:41:38 +0300

    remove duplicate power_to_db

--- caits/fe/_spectrum.py
+++ caits/fe/_spectrum.py
@@ -422,8 +422,8 @@ def mfcc_stats(
     **kwargs: Any,
 ) -> Union[np.ndarray, dict]:
 
-    mfcc_arr = mfcc(y=y, sr=sr, S=S, n_mfcc=n_mfcc, dct_type=dct_type,
-                        norm=norm, lifter=lifter, **kwargs)
+    mfcc_arr = mfcc(y=y, sr=sr, S=S, n_mfcc=n_mfcc,
+                    dct_type=dct_type, norm=norm, lifter=lifter, **kwargs)
     delta_arr = delta(mfcc_arr)
 
     mfcc_mean = np.mean(mfcc_arr, axis=1)
@@ -489,8 +489,8 @@ def mfcc(
     **kwargs: Any,
 ) -> np.ndarray:
     if S is None:
-    # multichannel behavior may be different due to relative noise floor
-    # differences between channels
+        # multichannel behavior may be different due to relative noise floor
+        # differences between channels
         S = power_to_db(melspectrogram(y=y, sr=sr, **kwargs))
 
     M: np.ndarray = scipy.fftpack.dct(S, axis=-2, type=dct_type, norm=norm)[
@@ -556,54 +556,6 @@ def power_to_db(
     # librosa v0.10.1:
     # https://github.com/librosa/librosa/blob/main/librosa/core/spectrum.py
 
-    S = np.asarray(S)
-
-    if amin <= 0:
-        raise ValueError("amin must be strictly positive")
-
-    if np.issubdtype(S.dtype, np.complexfloating):
-        warnings.warn(
-            "power_to_db was called on complex input so phase "
-            "information will be discarded. To suppress this warning, "
-            "call power_to_db(np.abs(D)**2) instead.",
-            stacklevel=2,
-        )
-        magnitude = np.abs(S)
-    else:
-        magnitude = S
-
-    if callable(ref):
-        # User supplied a function to calculate reference power
-        ref_value = ref(magnitude)
-    else:
-        ref_value = np.abs(ref)
-
-    log_spec: np.ndarray = 10.0 * np.log10(np.maximum(amin, magnitude))
-    log_spec -= 10.0 * np.log10(np.maximum(amin, ref_value))
-
-    if top_db is not None:
-        if top_db < 0:
-            raise ValueError("top_db must be non-negative")
-        log_spec = np.maximum(log_spec, log_spec.max() - top_db)
-
-    return log_spec
-
-
-def power_to_db(
-    S: np.ndarray,
-    *,
-    ref: Union[float, Callable] = 1.0,
-    amin: float = 1e-10,
-    top_db: Optional[float] = 80.0,
-) -> np.ndarray:
-    # The functionality in this implementation is basically derived from
-    # librosa v0.10.1:
-    # https://github.com/librosa/librosa/blob/main/librosa/core/spectrum.py
-    """Convert a power spectrogram (amplitude squared) to decibel (dB) units
-
-    This computes the scaling ``10 * log10(S / ref)`` in a numerically
-    stable way.
-    """
     S = np.asarray(S)
 
     if amin <= 0:
