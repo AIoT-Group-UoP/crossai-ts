71f3799747ab42e75fa5e57ced8bfff8ca292683
gkontogiannhs <g.kontogiannis@ac.upatras.gr>
2024-05-29 22:08:45 +0300

    CTS-69 feat: Update tf addons AdaptiveAveragePooling1D with new custom

--- caits/ai/__init__.py
+++ caits/ai/__init__.py
@@ -1,7 +1,9 @@
-from ._layers_dropout import (
+from ._layers import (
     MCDropout,
     MCSpatialDropout1D,
     MCSpatialDropout2D,
+    AdaptiveAveragePooling1D,
+    AdaptiveMaxPooling1D,
     dense_drop_block,
     dropout_layer_1d,
     dropout_layer_2d,
@@ -11,6 +13,8 @@ __all__ = [
     "MCDropout",
     "MCSpatialDropout1D",
     "MCSpatialDropout2D",
+    "AdaptiveAveragePooling1D",
+    "AdaptiveMaxPooling1D",
     "dropout_layer_1d",
     "dropout_layer_2d",
     "dense_drop_block",
