55140ffe18dd45a07e48709e66a6c440b50f22b6
gkontogiannhs <g.kontogiannis@ac.upatras.gr>
2024-06-17 13:56:00 +0300

    feat: modify handling of function and args for consistency
    At the moment, both FeatureExtractor2D and FunctionTransformer2D are very similar code-like and working, altough, in the future when multi-channel 2d transformations will be supported, FeatureExtractor2D will change drastically

--- caits/transformers/_feature_extractor_2d.py
+++ caits/transformers/_feature_extractor_2d.py
@@ -1,5 +1,4 @@
-from typing import Dict
-
+from typing import Dict, Callable, Any
 from pandas import DataFrame
 from sklearn.base import BaseEstimator, TransformerMixin
 
@@ -7,8 +6,9 @@ from ..dataset import Dataset
 
 
 class FeatureExtractor2D(BaseEstimator, TransformerMixin):
-    def __init__(self, feature_extractor: Dict):
-        self.feature_extractor = feature_extractor
+    def __init__(self, func: Callable, **kw_args: Dict[str, Any]):
+        self.func = func
+        self.kw_args = kw_args
 
     def fit(self, X, y=None):
         return self
@@ -21,20 +21,31 @@ class FeatureExtractor2D(BaseEstimator, TransformerMixin):
             if df.shape[1] != 1:
                 raise ValueError(
                     "2D Extractor does not support multi-channel signals yet. \
-                                 Expected single-channel signals."
+                    Expected single-channel signals."
                 )
 
-            func = self.feature_extractor["func"]
-            params = self.feature_extractor.get("params", {})
-
             # Extract information
-            feature = func(df.values.flatten(), **params)
+            feature = self.func(df.values.flatten(), **self.kw_args)
 
             # Ensure feature is a 2D array
             if feature.ndim != 2:
-                raise ValueError("Expected a 2D array.")
+                raise ValueError("Expected a 2D array after the extraction.")
 
             # Store the 2D numpy array as a DataFrame
             transformed_X.append(DataFrame(feature))
 
         return Dataset(transformed_X, X.y, X._id)
+
+    def get_params(self, deep=True):
+        """Overrides get_params to include func and kw_args."""
+        params = super().get_params(deep=deep)
+        params["func"] = self.func
+        params.update(self.kw_args)
+        return params
+
+    def set_params(self, **params):
+        """Overrides set_params to correctly handle func and kw_args."""
+        if "func" in params:
+            self.func = params.pop("func")
+        self.kw_args = params
+        return self
