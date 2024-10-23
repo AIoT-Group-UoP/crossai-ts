184ce0d60a2bf3e060001c8926882689eb8b0572
AIoT01 <tzamalis@ceid.upatras.gr>
2024-10-23 08:22:22 +0000

    add rolling feats, change loading to direct path

--- examples/utils.py
+++ examples/utils.py
@@ -1,32 +1,33 @@
-def load_wav_instance():
+def load_wav_instance(path, return_vector=True):
 
-    from caits.loading import load_yaml_config
     from caits.loading import wav_specs_check
     from caits.loading import wav_loader
 
-    config = load_yaml_config("config.yml")
-    audio_params = wav_specs_check("data/yes.wav")
-    audio_load = wav_loader("data/yes.wav")
+    audio_params = wav_specs_check(path)
+    audio_load = wav_loader(path)
+
+    if audio_params["nchannels"] == 1 and return_vector:
+        ch_name = list(audio_load[0].columns)[0]
+        sig = audio_load[0][ch_name].values
+    elif audio_params["nchannels"] == 1 and not return_vector:
+        ch_name = list(audio_load[0].columns)
+        sig = audio_load[0].values
 
     return {
-        "signal": audio_load[0].to_numpy(),
+        "signal": sig,
         "params": audio_params,
+        "channels": ch_name,
         "sr": audio_params["framerate"],
-        "shape": audio_load[0].shape
+        "shape": sig.shape
     }
 
 
-def load_csv_instance():
+def load_csv_instance(path):
     import pandas as pd
-    from caits.loading import load_yaml_config
-
-    config = load_yaml_config("config.yml")
-    df = pd.read_csv("data/scratching_eye.csv")
-    
+    df = pd.read_csv(path)
 
     return {
         "signal": df.to_numpy(),
-        "sr": config["sampling_rate"],
         "shape": df.shape,
         "channels": df.columns.tolist()
     }
