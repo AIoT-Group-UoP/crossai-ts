72c61a389749739f4d5a2cd05e64d12bf6752502
GiannopoulosKyr <kyrgiannopou@gmail.com>
2025-07-21 14:12:17 +0300

    #104 fix feature_extractor_2d_v2 to work with new dataset objects

--- caits/transformers/_feature_extractor_2d_v2.py
+++ caits/transformers/_feature_extractor_2d_v2.py
@@ -2,9 +2,9 @@ from typing import Dict, Callable, Any, Union, TypeVar
 from pandas import DataFrame
 from sklearn.base import BaseEstimator, TransformerMixin
 
-from ..dataset import Dataset2
+from caits.dataset._dataset3 import Dataset3
 
-T = TypeVar('T', bound="Dataset2")
+T = TypeVar('T', bound="Dataset3")
 
 
 class FeatureExtractor2D(BaseEstimator, TransformerMixin):
@@ -16,25 +16,28 @@ class FeatureExtractor2D(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, data: T) -> T:
-        transformed_X = []
+        # transformed_X = []
+        #
+        # for d in data.X:
+        #     transformed_X.append({})
+        #     for col_name, col_values in d.items():
+        #         # Extract information
+        #         feature = self.func(col_values, **self.kw_args)
+        #
+        #         # Ensure feature is a 2D array
+        #         if feature.ndim != 2:
+        #             raise ValueError("Expected a 2D array after the extraction.")
+        #
+        #         # Store the 2D numpy array as a DataFrame
+        #         transformed_X[-1][col_name] = feature
+        #
+        # tmp = data.to_dict()
+        # tmp["X"] = transformed_X
+        #
+        # return data.__class__(**tmp)
 
-        for d in data.X:
-            transformed_X.append({})
-            for col_name, col_values in d.items():
-                # Extract information
-                feature = self.func(col_values, **self.kw_args)
-
-                # Ensure feature is a 2D array
-                if feature.ndim != 2:
-                    raise ValueError("Expected a 2D array after the extraction.")
-
-                # Store the 2D numpy array as a DataFrame
-                transformed_X[-1][col_name] = feature
-
-        tmp = data.to_dict()
-        tmp["X"] = transformed_X
-
-        return data.__class__(**tmp)
+        features = data.apply(self.func, **self.kw_args)
+        return features
 
 
     def get_params(self, deep=True):
