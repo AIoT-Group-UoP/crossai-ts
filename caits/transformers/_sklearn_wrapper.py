f646be43e0aedc61de7b93505ead54a994cdeff2
GiannopoulosKyr <kyrgiannopou@gmail.com>
2025-07-31 16:56:02 +0300

    #104 feat: SklearnWrapper transformer

---
+++ caits/transformers/_sklearn_wrapper.py
@@ -0,0 +1,73 @@
+from typing import Union, TypeVar, List, Dict
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import FunctionTransformer
+
+from caits.dataset._dataset3 import Dataset3
+
+T = TypeVar('T', bound="Dataset3")
+
+class SklearnWrapper(BaseEstimator, TransformerMixin):
+    def __init__(self, transformer, **transformer_kwargs):
+        """Initializes the Transformer class.
+
+        Args:
+            func: A function that will be applied column-wise to each
+                  DataFrame.
+            **func_kwargs: Keyword arguments to be passed to the function.
+        """
+        self.transformer = transformer
+        self.transformer_kwargs = transformer_kwargs
+
+    def fit(self, X, y=None):
+        """Fits the transformer
+
+        Args:
+            X: The input data (ignored).
+            y: The target values (ignored).
+
+        Returns:
+            self: Returns the instance itself.
+        """
+        self.fitted_transformer_ = self.transformer(**self.transformer_kwargs).fit(X.X.values, y)
+        return self
+
+    def transform(self, data: T) -> T:
+        """Applies the transformation function column-wise to the data.
+
+        Args:
+            X: The Dataset object containing the data to be transformed.
+
+        Returns:
+            Dataset: A new Dataset object with the transformed data.
+        """
+        transformed_X = self.fitted_transformer_.transform(data.X.values)
+        return data.numpy_to_dataset(transformed_X, split=False)
+
+    def get_params(self, deep=True):
+        """Overrides get_params to include func_kwargs.
+
+        Args:
+            deep (bool): If True, will return the parameters for this
+                         estimator and contained subobjects that are
+                         estimators.
+
+        Returns:
+            dict: Parameters of the estimator.
+        """
+        params = super().get_params(deep=deep)
+        params.update(self.transformer_kwargs)
+        return params
+
+    def set_params(self, **params):
+        """Overrides set_params to correctly handle func_kwargs.
+
+        Args:
+            **params: Parameter names mapped to their values.
+
+        Returns:
+            self: The instance with updated parameters.
+        """
+        if "transformer" in params:
+            self.transformer = params.get("transformer", FunctionTransformer)
+            self.transformer_kwargs = params.get("transformer_kwargs", {})
+        return self
