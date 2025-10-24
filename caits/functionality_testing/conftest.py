421a54a0ed521c77b1f104fb3d7ebc0b86781513
GiannopoulosKyr <kyrgiannopou@gmail.com>
2025-10-24 14:40:28 +0000

    #104 add: fixture for sample array data with different shapes

--- caits/functionality_testing/conftest.py
+++ caits/functionality_testing/conftest.py
@@ -1,12 +1,10 @@
-from _pytest.reports import TestReport
 from _pytest.terminal import TerminalReporter
-from _pytest._code.code import ReprEntry, ExceptionChainRepr, ReprTraceback
 from colorama import init
-
-import config
+from caits.functionality_testing import config
+import numpy as np
+import pytest
 
 init(autoreset=True)
-
 def pytest_terminal_summary(terminalreporter: TerminalReporter):
     terminalreporter.section("Functionality Test", blue=True, bold=True)
 
@@ -19,3 +17,16 @@ def pytest_terminal_summary(terminalreporter: TerminalReporter):
     terminalreporter.write_line(config.FAIL_COLOR + f"✘ Failed: {failed}" + config.RESET)
     terminalreporter.write_line(config.ERROR_COLOR + f"! Error: {error}" + config.RESET)
     terminalreporter.write_line(config.SKIP_COLOR + f"➜ Skipped: {skipped}" + config.RESET)
+
+
+params = [
+    ((1000,), 0),
+    ((1000, 1), 0),
+    ((1000, 2), 0),
+    ((1, 1000), 1),
+    ((2, 1000), 1),
+]
+
+@pytest.fixture(params=params, ids=lambda arr: f"shape={arr[0]}-axis={arr[1]}")
+def arr(request):
+    return np.random.rand(*request.param[0]), request.param[1]
