958742ad85d292eb98ef9f3e7cc9a2ca45ff3d37
GiannopoulosKyr <kyrgiannopou@gmail.com>
2025-10-24 14:39:49 +0000

    #104 fix: remove unecessary runtests.py file

--- caits/functionality_testing/runtests.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-import pytest
-
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
-sys.path.insert(0, project_root)
-
-pytest.main(args=["--tb=short", "-s"])
