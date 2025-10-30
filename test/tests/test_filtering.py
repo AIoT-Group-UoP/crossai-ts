ae44dff88fd6f468fa5fb1d59b1fd1583c1d2e9d
GiannopoulosKyr <kyrgiannopou@gmail.com>
2025-10-30 16:33:08 +0000

    #104 test: test_filtering.py

--- test/tests/test_filtering.py
+++ test/tests/test_filtering.py
@@ -1,115 +1,145 @@
-import numpy as np
 import caits.filtering as filtering
-import pytest
-import itertools
-
-# Median simple
-kernel_size = [x for x in range(1, 7, 2)]
-
-# Median gen
-window_size = [x for x in range(1, 10)]
-
-# Butterworth
-fs = [x for x in range(100, 1000, 100)]
-fs_cutoff_combs = [
-    (f, c)
-    for f in fs for c in range(f // 10, f // 2, f // 20)
-]
-filter_type = ["lowpass", "highpass"]
-
-order = [x for x in range(1, 10)]
-zi_enable = [True, False]
-butterworth_method = ['filtfilt', 'sosfilt', 'sosfiltfilt']
-
-# Gaussian
-sigma = [x for x in range(1, 4)]
-gaussian_mode = ['reflect', 'constant', 'nearest', 'mirror', 'wrap']
-truncate = [x * 0.5 for x in range(9)]
-cval = [x * 0.5 for x in range(9)]
-
-filter_median_simple_combs = kernel_size
-filter_median_gen_combs = window_size
-filter_butterworth_lp_hp_combs = list(itertools.product(fs_cutoff_combs, filter_type))
-filter_gaussian_combs = list(itertools.product(sigma, gaussian_mode, truncate, cval))
-
-bp_cutoff_combs = [
-    ((f, (l, h)), "bandpass")
-    for f in fs
-    for h in range(f // 10, f // 2, f // 30)
-    for l in range(h // 10, h // 2, h // 10)
-]
-
-filter_butterworth_bound_combs = filter_butterworth_lp_hp_combs + bp_cutoff_combs
-filter_butterworth_combs = itertools.product(filter_butterworth_bound_combs, order, zi_enable, butterworth_method)
+from test.utils import make_test, total_gen
 
 
-filter_median_simple_kwargs = [
+def gen_kernel_size(max_size):
+    for x in range(1, max_size+1, 2):
+        yield x
+
+def gen_window_size(max_size):
+    for x in range(1, max_size+1):
+        yield x
+
+def gen_fs(min_fs, max_fs, step):
+    for x in range(min_fs, max_fs+step, step):
+        yield x
+
+def gen_filter_type():
+    for t in ["lowpass", "highpass", "bandpass"]:
+        yield t
+
+def gen_cutoff(fs, filter_type):
+    if filter_type != "bandpass":
+        for f in range(fs // 10, fs // 2, fs // 20):
+            yield f
+    else:
+        for h in range(fs // 10, fs // 2, fs // 30):
+            for l in range(h // 10, h // 2, h // 10):
+                yield l, h
+
+def gen_order(max_order):
+    for x in range(1, max_order):
+        yield x
+
+def gen_zi_enable():
+    yield True
+    yield False
+
+def gen_butterworth_method():
+    for x in ["filtfilt", "sosfilt", "sosfiltfilt"]:
+        yield x
+
+def gen_sigma():
+    for x in range(1, 4):
+        yield x
+
+def gen_gaussian_mode():
+    for x in ["reflect", "constant", "nearest", "mirror", "wrap"]:
+        yield x
+
+def gen_truncate():
+    for x in range(9):
+        yield 0.5 * x
+
+def gen_cval():
+    for x in range(9):
+        yield 0.5 * x
+
+
+filter_median_simple_params = [
     {
-        "kernel_size": ws
+        "name": "kernel_size",
+        "func": gen_kernel_size,
+        "kwargs": {
+            "max_size": 7
+        }
     }
-    for ws in filter_median_simple_combs
 ]
 
-filter_median_gen_kwargs = [
+filter_median_gen_params = [
     {
-        "window_size": ws
+        "name": "window_size",
+        "func": gen_window_size,
+        "kwargs": {
+            "max_size": 10
+        }
     }
-    for ws in filter_median_gen_combs
-]
-
-filter_butterworth_kwargs = [
-    {
-        "fs": c[0][0][0],
-        "filter_type": c[0][1],
-        "cutoff_freq": c[0][0][1],
-        "order": c[1],
-        "zi_enable": c[2],
-        "method": c[3]
-    }
-    for c in filter_butterworth_combs
-]
-
-filter_gaussian_kwargs = [
-    {
-        "sigma": c[0],
-        "mode": c[1],
-        "truncate": c[2],
-        "cval": c[3]
-    }
-    for c in filter_gaussian_combs
-]
-
-
-simple_median_params = [
-    pytest.param(
-        kwargs,
-        id="-".join([f"{k}={v}" for k, v in kwargs.items()])
-    )
-    for kwargs in filter_median_simple_kwargs
-]
-
-median_gen_params = [
-    pytest.param(
-        kwargs,
-        id="-".join([f"{k}={v}" for k, v in kwargs.items()])
-    )
-    for kwargs in filter_median_gen_kwargs
 ]
 
 filter_butterworth_params = [
-    pytest.param(
-        kwargs,
-        id="-".join([f"{k}={v}" for k, v in kwargs.items()])
-    )
-    for kwargs in filter_butterworth_kwargs
+    {
+        "name": "fs",
+        "func": gen_fs,
+        "kwargs": {
+            "min_fs": 100,
+            "max_fs": 1000,
+            "step": 100
+        }
+    },
+    {
+        "name": "filter_type",
+        "func": gen_filter_type,
+        "kwargs": {}
+    },
+    {
+        "name": "cutoff_freq",
+        "func": gen_cutoff,
+        "kwargs": {
+            "fs": "from_fs",
+            "filter_type": "from_filter_type"
+        }
+    },
+    {
+        "name": "order",
+        "func": gen_order,
+        "kwargs": {
+            "max_order": 10
+        }
+    },
+    {
+        "name": "zi_enable",
+        "func": gen_zi_enable,
+        "kwargs": {}
+    },
+    {
+        "name": "method",
+        "func": gen_butterworth_method,
+        "kwargs": {}
+    }
 ]
 
+
 filter_gaussian_params = [
-    pytest.param(
-        kwargs,
-        id="-".join([f"{k}-{v}" for k, v in kwargs.items()])
-    )
-    for kwargs in filter_gaussian_kwargs
+    {
+        "name": "sigma",
+        "func": gen_sigma,
+        "kwargs": {}
+    },
+    {
+        "name": "mode",
+        "func": gen_gaussian_mode,
+        "kwargs": {}
+    },
+    {
+        "name": "truncate",
+        "func": gen_truncate,
+        "kwargs": {}
+    },
+    {
+        "name": "cval",
+        "func": gen_cval,
+        "kwargs": {}
+    }
 ]
 
 funs = [
@@ -119,17 +149,13 @@ funs = [
     filtering.filter_gaussian
 ]
 
-params = [simple_median_params, median_gen_params, filter_butterworth_params, filter_gaussian_params]
+params = [
+    total_gen(filter_median_simple_params),
+    total_gen(filter_median_gen_params),
+    total_gen(filter_butterworth_params),
+    total_gen(filter_gaussian_params)
+]
 
-
-def _make_test(fun, params):
-    @pytest.mark.parametrize(
-        "kwargs", params
-    )
-    def test_func(arr, kwargs):
-        assert fun(arr[0], axis=arr[1], **kwargs).shape == arr[0].shape
-
-    return test_func
 
 for fun, params in zip(funs, params):
-    globals()[f"test_{fun.__name__}"] = _make_test(fun, params)
+    globals()[f"test_{fun.__name__}"] = make_test(fun, params)
