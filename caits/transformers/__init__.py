02766f6e81b265540cd07008c86501e898daeda2
GiannopoulosKyr <kyrgiannopou@gmail.com>
2025-11-12 11:19:25 +0000

    #108 fix: remove FeatureExtractor and FeatureExtractor2D from transformers/__init__.py (deprecated)

--- caits/transformers/__init__.py
+++ caits/transformers/__init__.py
@@ -2,8 +2,8 @@
 from ._augmentation_1d import Augmenter1D
 # from ._data_converters import ArrayToDataset, DatasetToArray
 from ._encoder import LE
-from ._feature_extractor import FeatureExtractor
-from ._feature_extractor_2d import FeatureExtractor2D
+# from ._feature_extractor import FeatureExtractor
+# from ._feature_extractor_2d import FeatureExtractor2D
 # from ._func_transformer import FunctionTransformer
 # from ._sliding_window import SlidingWindow
 # from ._func_transformer_2d import FunctionTransformer2D
