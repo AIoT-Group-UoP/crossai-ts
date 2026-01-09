05c251fdb1ef5bd14c3c0626db2ce1514ffafcec
GiannopoulosKyr <kyrgiannopou@gmail.com>
2026-01-09 16:17:30 +0000

    #108 refactor: change SKIP test color

--- test/config.py
+++ test/config.py
@@ -3,6 +3,6 @@ from colorama import Fore, Style
 INFO_COLOR = Fore.CYAN + Style.BRIGHT
 SUCCESS_COLOR = Fore.GREEN
 FAIL_COLOR = Fore.RED + Style.BRIGHT
-SKIP_COLOR = Fore.CYAN
+SKIP_COLOR = Fore.WHITE
 ERROR_COLOR = Fore.YELLOW + Style.BRIGHT
 RESET = Style.RESET_ALL
