f8ee22180dd98255d13bb4500b0fa893cb136c40
GiannopoulosKyr <kyrgiannopou@gmail.com>
2026-01-13 14:07:05 +0000

    #109 add: SklearnWrapper Pipeline class that handles shape of input and output

--- caits/transformers/_sklearn_wrapper.py
+++ caits/transformers/_sklearn_wrapper.py
@@ -1,6 +1,8 @@
 from typing import Union, TypeVar, List, Dict, Optional
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import FunctionTransformer
+from sklearn.pipeline import Pipeline
+from ._data_converters_v2 import DatasetToArray, ArrayToDataset
 from ..dataset import DatasetBase
 T = TypeVar('T', bound="DatasetBase")
 
@@ -96,3 +98,67 @@ class SklearnStep(BaseEstimator, TransformerMixin):
             self.transformer = params.get("transformer", FunctionTransformer)
             self.transformer_kwargs = params.get("transformer_kwargs", {})
         return self
+
+
+class SklearnWrapper(Pipeline):
+    def __init__(
+            self,
+            sklearn_transformers,
+            to_X=True,
+            to_y=False,
+    ):
+
+        self.to_X = to_X
+        self.to_y = to_y
+        self.sklearn_transformers = [
+            (name, SklearnStep(skt, to_X=to_X, to_y=to_y, **params))
+            for name, skt, params in sklearn_transformers
+        ]
+        super().__init__(self.sklearn_transformers)
+
+
+    def fit(
+            self,
+            X,
+            y=None,
+            **params
+    ):
+        """Fits the transformer
+        Args:
+            X: The input data (ignored).
+        """
+        self.shape_X_ = X.X.shape
+        self.shape_y_ = X.y.shape
+
+        reshaper = (
+            "reshaper",
+            DatasetToArray(
+                flatten=True,
+                to_X=self.to_X,
+                to_y=self.to_y
+            )
+        )
+
+        inverseReshaper = (
+            "inverseReshaper",
+            ArrayToDataset(
+                shape_X=self.shape_X_,
+                shape_y=self.shape_y_,
+                to_X=self.to_X,
+                to_y=self.to_y,
+                axis_names={
+                    "X": X.X.keys(),
+                    "y": X.y.keys()
+                },
+                flattened=True
+            )
+        )
+
+        self.steps = [
+            reshaper,
+            *self.sklearn_transformers,
+            inverseReshaper
+        ]
+
+        super().fit(X, y, **params)
+
