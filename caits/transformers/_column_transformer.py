20189839d8bc774549438995be57497714ec3465
GiannopoulosKyr <kyrgiannopou@gmail.com>
2026-01-23 13:12:39 +0000

    #110 fix: ColumnTransformer.transform correct y handling

--- caits/transformers/_column_transformer.py
+++ caits/transformers/_column_transformer.py
@@ -24,25 +24,41 @@ class ColumnTransformer(BaseEstimator, TransformerMixin):
 
         return self
 
-    # TODO: Maybe not replace
+    # TODO: Replaces to the original data we do not want that
     def transform(self, data: T) -> Union[T, Dict]:
 
         tr_data = []
         if self.unify:
-            column_names_arr = sum([t[-1] for t in self.transformations_], [])
-            tr_data.append(data)
-            column_names_arr = list(data.get_axis_names_X()["axis_1"].keys()) + column_names_arr
-        else:
-            column_names_arr = sum([t[-2] for t in self.transformations_], [])
+            column_names_arr_X = sum([t[-1]["X"][1] if "X" in t[-1] else [] for t in self.transformations_], [])
+            column_names_arr_y = sum([t[-1]["y"][1] if "y" in t[-1] else [] for t in self.transformations_], [])
 
-        column_names = {col: i for i, col in enumerate(column_names_arr)}
+            tr_data.append(data)
+            column_names_arr_X = list(data.get_axis_names_X()["axis_1"].keys()) + column_names_arr_X
+            column_names_arr_y = list(data.y.keys()["axis_1"]) + column_names_arr_y
+        else:
+            column_names_arr_X = sum([t[-1]["X"][0] if "X" in t[-1] else [] for t in self.transformations_], [])
+            column_names_arr_y = sum([t[-1]["y"][0] if "y" in t[-1] else [] for t in self.transformations_], [])
+
+        column_names_X = {col: i for i, col in enumerate(column_names_arr_X)}
+        column_names_y = {col: i for i, col in enumerate(column_names_arr_y)}
 
         for transformation in self.transformations_:
-            name, transformer, columns, new_columns = transformation
-            _data = data[:, columns]
+            name, transformer, columns_set = transformation
+
+            columns_X = columns_set["X"][0] if "X" in columns_set else []
+            columns_y = columns_set["y"][0] if "y" in columns_set else []
+
+            _data = data[:, columns_X + columns_y]
             tr_data.append(transformer.transform(_data))
 
-        final_tr_data = tr_data[0].unify(tr_data[1:], axis_names={"axis_1": column_names}, axis=1)
+        final_tr_data = tr_data[0].unify(
+            tr_data[1:],
+            axis_names={
+                "X": {"axis_1": column_names_X},
+                "y": {"axis_1": column_names_y}
+            },
+            axis=1
+        )
 
         if self.unify:
             return final_tr_data
