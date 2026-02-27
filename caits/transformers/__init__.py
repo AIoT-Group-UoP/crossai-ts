de145411836534c3178ac07763706e953b03e0f9
GiannopoulosKyr <kyrgiannopou@gmail.com>
2026-02-27 16:57:40 +0000

    #110 refactor: transformers/__init__.py

--- caits/transformers/__init__.py
+++ caits/transformers/__init__.py
@@ -2,12 +2,9 @@ from ._data_converters import ArrayToDataset, DatasetToArray
 from ._encoder import LE
 from ._sliding_window import SlidingWindow
 from ._augment_signal import AugmentSignal
-from ._scalar import FeatureExtractorScalar
-from ._signal import FeatureExtractorSignal
-from ._spectrum import FeatureExtractorSpectrum
+from ._feature_extractors import FeatureExtractorScalar, FeatureExtractorSignal, FeatureExtractorSpectrum
 from ._column_transformer import ColumnTransformer
-from ._func_transformer import FunctionTransformer
-from ._func_transformer_2d import FunctionTransformer2D
+from ._function_transformers import FunctionTransformer, FunctionTransformer2D
 from ._sklearn_wrapper import SklearnPipeStep, SklearnWrapper
 
 __all__ = [
