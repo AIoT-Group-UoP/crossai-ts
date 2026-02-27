eb96386589d53860bd64eba0b7f10e0792746838
GiannopoulosKyr <kyrgiannopou@gmail.com>
2026-02-27 16:56:36 +0000

    #110 refactor: moved feature extractor transformers to dedicated submodule

---
+++ caits/transformers/_feature_extractors/__init__.py
@@ -0,0 +1,9 @@
+from ._scalar import FeatureExtractorScalar
+from ._signal import FeatureExtractorSignal
+from ._spectrum import FeatureExtractorSpectrum
+
+__all__ = [
+    "FeatureExtractorScalar",
+    "FeatureExtractorSignal",
+    "FeatureExtractorSpectrum",
+]
