d88f30c85977d1a96a2771c45b4f8acba61ae646
GiannopoulosKyr <kyrgiannopou@gmail.com>
2026-03-06 11:08:48 +0000

    #111 feat: add dataset._convert.concat method to be pandas and numpy like

--- caits/dataset/_convert.py
+++ caits/dataset/_convert.py
@@ -43,6 +43,13 @@ def stack(data):
     )
 
 
+def concat(data, axis=0, to_X=True, to_y=False):
+    if not all(d.__class__.__name__ == data[0].__class__.__name__ for d in data[1:]):
+        raise ValueError("Data objects have to be of the same type")
+
+    return data[0].__class__.concat(init_data=data, axis=axis, to_X=to_X, to_y=to_y)
+
+
 def reshape(
         data,
         shape_X,
